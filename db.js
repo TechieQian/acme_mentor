d810a5ed7a4eccab3886f3c9b93e0456cd906ac6
Qian Wan <qiansan@gmail.com>
2017-08-16 00:28:27 -0400

    done

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost:5432/mentordb');
+const db = new Sequelize(process.env.DATABASE_URL);
 
 var User = db.define('user', {
   name: {
